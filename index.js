e4815c3202351cc4dcf9fb7cf20c828848c529b0
swethakrishnan <swethapt333@gmail.com>
2022-01-22 12:03:34 +0530

    added post requit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var express = require('express')
-var cors = require('cors')
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(express.static('pubic'))
 app.use(bodyParser.urlencoded({ extended: true }))
-const url = '/mongodb://localhost:27017'
+const url = '/mongodb://localhost:27017/userStore'
+
+const database = mongoose.connection;
+
+app.post("/signup", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+    const data = { name, email, phone, password }
+})
 
 
 mongoose.connect()
