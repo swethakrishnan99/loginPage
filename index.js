f4ab12c85d555183457ff418f92eecec656b8bda
swethakrishnan <swethapt333@gmail.com>
2022-01-23 23:17:19 +0530

    added frontend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,16 +15,35 @@ const database = mongoose.connection;
 database.on('error', () => console.log("Error in Connecting to Database"));
 database.once('open', () => console.log("Connected to Database"))
 
-app.post("/sign_up", (req, res) => {
+app.post("/sign_up", async (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const phone = req.body.phone;
     const password = req.body.password;
     const data = { name, email, phone, password };
+    const duplicate = await database.collection("users").findOne({ email }, (err, data) => {
+        if (err) throw err;
+        return data
+    });
+    if (duplicate) res.sendStatus(409)
     database.collection("users").insertOne(data, (err, res) => {
         if (err) throw err;
-        console.log("Record inserted successfully");
+
     });
+    res.sendStatus(200)
+});
+
+app.post("/sign_in", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const data = { email, password };
+    const response = { user: false }
+    database.collection("users").findOne(data, (err, res) => {
+        if (err) throw err;
+        console.log(response);
+        (res === null) ? response.user = false : response.user = true;
+    });
+
     return (res.redirect('success.html'))
 });
 
@@ -32,4 +52,4 @@ app.get("/", (req, res) => {
     res.redirect("index.html")
 });
 
-app.listen(8081, console.log("server started on port 8081"));
+app.listen(8000, console.log("server started on port 8000"));
---
+++ public/script.js
@@ -0,0 +1,36 @@
+// const axios = require('axios');
+
+const sign = document.getElementsByClassName('container')
+const log = document.getElementsByClassName('wrapper')
+sign[0].style.display = "none"
+const register = document.getElementById("reg-form");
+register.addEventListener('submit', event => {
+    event.preventDefault();
+    regForm()
+});
+
+function signupage() {
+    log[0].style.display = "none"
+    sign[0].style.display = "block"
+}
+
+function loginpage() {
+    log[0].style.display = "block"
+    sign[0].style.display = "none"
+}
+
+function regForm() {
+    const name = document.getElementById('reg-name').value;
+    const password = document.getElementById('reg-password').value;
+    const email = document.getElementById('reg-email').value;
+    const phone = document.getElementById('reg-phone').value;
+    const data = { name, email, password, phone }
+    axios
+        .post("/sign_up", data)
+        .then((res) => {
+            console.log("RESPONSE RECEIVED: ", res);
+        })
+        .catch((err) => {
+            console.log("AXIOS ERROR: ", err);
+        });
+}
